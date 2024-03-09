b9e7f91df4b26f5f624feb76dc05f9f5fa27ae8a
Irfan Marzenda <irfan.marzenda@gmail.com>
2024-03-09 16:51:12 +0700

    add footer and image
    menambahkan sebuah footer pada website lpk imiah dan menambahkan gambar asset logo dan konten untuk sementara

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-white mt-3 navbar-dark " style="background-color: #0E6AC7" >
+    <div class="container">
+        <div class="row pt-3">
+            <div class="col text-center">
+                <p>Copyright &copy; 2024 : LPK IMIAH - All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
