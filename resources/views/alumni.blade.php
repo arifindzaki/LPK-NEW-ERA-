9e076954d74e8195b192398034c4b477d831ace7
Rica Rahmahidayatul <ricarahma97@gmail.com>
2024-03-26 10:47:04 +0700

    halaman alumni

--- resources/views/alumni.blade.php
+++ resources/views/alumni.blade.php
@@ -1,5 +1,167 @@
 @extends('layout.main')
 
 @section('container')
-<p>Di isi konten alumni</p>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-header">
+                <h2>Alumni</h2>
+                <p>Ilmu Berkualitas, Pengalaman Berbicara di LPK ILMIAH </p>
+            </div>
+
+            <div class="slides-3 swiper" data-aos="fade-up" data-aos-delay="100">
+                <div class="swiper-wrapper">
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-wrap">
+                            <div class="testimonial-item">
+                                <h3 class="text-center mb-4">Your Title Here</h3>
+                                <div class="d-flex flex-column align-items-center">
+                                    <img src="img/WEE.jpeg" class="testimonial-img img-fluid mb-3" alt="">
+                                    <div>
+                                        {{-- isi --}}
+                                        <div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h4 class="text-center">Bekerja di PT A</h4>
+                                <p class="text-center">
+                                    Belajar di LPK IMIAH membantu saya mengasah skill saya dibidang DKV hingga saya diterima
+                                    kerja.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-wrap">
+                            <div class="testimonial-item">
+                                <h3 class="text-center mb-4">Your Title Here</h3>
+                                <div class="d-flex flex-column align-items-center">
+                                    <img src="img/about.jpg" class="testimonial-img img-fluid mb-3" alt="">
+                                    <div>
+                                        {{-- isi --}}
+                                        <div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h4 class="text-center">Bekerja di PT A</h4>
+                                <p class="text-center">
+                                    Belajar di LPK IMIAH membantu saya mengasah skill saya dibidang DKV hingga saya diterima
+                                    kerja.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-wrap">
+                            <div class="testimonial-item">
+                                <h3 class="text-center mb-4">Your Title Here</h3>
+                                <div class="d-flex flex-column align-items-center">
+                                    <img src="img/about.jpg" class="testimonial-img img-fluid mb-3" alt="">
+                                    <div>
+                                        {{-- isi --}}
+                                        <div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h4 class="text-center">Bekerja di PT A</h4>
+                                <p class="text-center">
+                                    Belajar di LPK IMIAH membantu saya mengasah skill saya dibidang DKV hingga saya diterima
+                                    kerja.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-wrap">
+                            <div class="testimonial-item">
+                                <h3 class="text-center mb-4">Your Title Here</h3>
+                                <div class="d-flex flex-column align-items-center">
+                                    <img src="img/about.jpg" class="testimonial-img img-fluid mb-3" alt="">
+                                    <div>
+                                        {{-- isi --}}
+                                        <div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h4 class="text-center">Bekerja di PT A</h4>
+                                <p class="text-center">
+                                    Belajar di LPK IMIAH membantu saya mengasah skill saya dibidang DKV hingga saya diterima
+                                    kerja.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-wrap">
+                            <div class="testimonial-item">
+                                <h3 class="text-center mb-4">Your Title Here</h3>
+                                <div class="d-flex flex-column align-items-center">
+                                    <img src="img/about.jpg" class="testimonial-img img-fluid mb-3" alt="">
+                                    <div>
+                                        {{-- isi --}}
+                                        <div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h4 class="text-center">Bekerja di PT A</h4>
+                                <p class="text-center">
+                                    Belajar di LPK IMIAH membantu saya mengasah skill saya dibidang DKV hingga saya diterima
+                                    kerja.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+
+        </div>
+    </section><!-- End Testimonials Section -->
+    
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
 @endsection
