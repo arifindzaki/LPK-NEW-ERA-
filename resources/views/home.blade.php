1478152c4ce9a01e4873fa02c0a7d34d3d99cf43
Irfan Marzenda <irfan.marzenda@gmail.com>
2024-03-31 01:19:26 +0700

    add content home
    add fasilitas, sertifikat in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,9 +70,9 @@
         <!-- Slide 2 -->
         <div class="carousel-item ">
             <div class="carousel-container">
-            <h2 class="animate__animated animate__fadeInDown">Benefit yang akan didapat!</h2>
+            <h2 class="animate__animated animate__fadeInDown">Mengapa Harus LPK ILMIAH?</h2>
             <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-            <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Selengkapnya</a>
+            <a href="#sertifikat" class="btn-get-started animate__animated animate__fadeInUp scrollto">Selengkapnya</a>
             </div>
         </div>
 
@@ -81,7 +81,7 @@
             <div class="carousel-container">
             <h2 class="animate__animated animate__fadeInDown">Bermacam Pelatihan di LPK IMLIAH</h2>
             <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-            <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+            <a href="#pelatihan" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
             </div>
         </div>
 
@@ -113,8 +113,8 @@
     </section><!-- End Hero -->
 
     <div class="container mt-4">
-        <div class="landing-page">
-            <div class="content">
+        <div id="about" class="landing-page">
+            <div class="content" data-aos-duration="1000">
                 <h4>Latar Belakang</h4>
                 <h1 style="color: rgb(58, 58, 243)">Selamat Datang di LPK ILMIAH</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae tempora tenetur sunt animi maxime rem exercitationem ipsa in repudiandae. Perspiciatis, labore alias quo aperiam nam illum ullam ratione! Excepturi, qui?Nesciunt, beatae suscipit. Dignissimos voluptatibus, asperiores esse impedit labore aut repellat quia at, eius debitis ab blanditiis iste vero nam ea enim aliquam, dolore exercitationem excepturi eos iure modi. Reprehenderit.</p>
@@ -122,6 +122,90 @@
         </div>
     </div>
 
+    {{-- Gambar --}}
+    <div class="" id="sertifikat">
+    <div class="text-center pt-4 mb-3" data-aos="fade-up" data-aos-duration="1000">
+        <img src="img/sertifikat.png" class="img-thumbnail m-3" style="max-width: 70vw; border-radius: 25px;">
+    </div>
+</div>
+    {{-- pelatihan --}}
+    <div class="container" data-aos="fade-up" data-aos-duration="1000">
+        <h2 style="text-align: center" id="pelatihan" >Pelatihan</h2>
+    <div class="row gx-4 gx-lg-5">
+        <div class="col-12">
+            {{-- <h1 class="text-center">Pelatihan</h1> --}}
+        </div>
+        
+        <div class="col-md-4 mb-5">
+            <div class="card h-100">
+                
+                <img src="img/about.jpg" class="card-img-top" alt="Pelatihan Desain Grafis">
+                <div class="card-body">
+                    <h2 class="card-title">Pelatihan Komputer</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                </div>
+                <div class="card-footer"><a class="btn btn-primary btn-sm" href="isipelatihan.php">More Info</a></div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-5">
+            <div class="card h-100">
+                <img src="img/about.jpg" class="card-img-top" alt="Pelatihan Desain Grafis">
+                <div class="card-body">
+                    <h2 class="card-title">Pelatihan Desain Grafis</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                </div>
+                <div class="card-footer"><a class="btn btn-primary btn-sm" href="isipelatihan.php">More Info</a></div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-5">
+            <div class="card h-100">
+                <img src="img/about.jpg" class="card-img-top" alt="Pelatihan Desain Grafis">
+                <div class="card-body">
+                    <h2 class="card-title">Pelatihan pemrogaman</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                </div>
+                <div class="card-footer"><a class="btn btn-primary btn-sm" href="isipelatihan.php">More Info</a></div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    {{-- Gambar --}}
+    <div class="text-center pt-4 mb-3" data-aos="fade-up" data-aos-duration="800">
+        <img src="img/fasilitas.png" class="img-thumbnail m-3" style="max-width: 70vw; border-radius: 25px;">
+    </div>
+    {{-- galery 3--}}
+    <div class="container">
+        <h2 data-aos="fade-up" data-aos-duration="900" style="text-align: center">Galery</h2>
+
+        <div class="row portfolio-container" data-aos="fade-up">
+        <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-img"><img src="img/bg biru.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info " style="text-align: center; background-color: #1068ec">
+            <h4>Gambar 1</h4>
+            <p>ini adalah gambar 1</p>
+            </div>
+        </div>
+
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-img"><img src="img/bg biru.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info" style="text-align: center; background-color: #1068ec">
+                <h4>Gambar 2</h4>
+                <p>ini adalah gambar 2</p>
+            </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-img"><img src="img/bg biru.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info" style="text-align: center; background-color: #1068ec">
+                <h4>Gambar 3</h4>
+                <p>Ini adalah Gamabr 3</p>
+            </div>
+            </div>
+        
+        </div>
+    </div>
 
     <footer class="text-white mt-3 navbar-dark " style="background-color: #0E6AC7" >
         <div class="container">
